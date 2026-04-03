9be24edf5b551e63dc02e515f8b4da83ba8762c6
이준용 <@172.30.1.53>
2026-04-03 19:47:25 +0900

    20260403

--- src/main/java/org/zerock/config/WebConfig.java
+++ src/main/java/org/zerock/config/WebConfig.java
@@ -4,22 +4,18 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 
 public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
 
-	@Override
-	protected Class<?>[] getRootConfigClasses() {
-		// TODO Auto-generated method stub
-		return new Class[] {RootConfig.class};
-	}
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { RootConfig.class };
+    }
 
-	@Override
-	protected Class<?>[] getServletConfigClasses() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] { ServletConfig.class };
+    }
 
-	@Override
-	protected String[] getServletMappings() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
 }
